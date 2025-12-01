9a9515c7ecb658640ec5427a3630786ba7abc3da
Ajburdett <ab64@sanger.ac.uk>
2025-12-01 22:53:36 +0000

    Day 1 Part 1 done

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-git
+
+
+lock_positions = range(100)
+current_position = 50
+zero_count = 0
+
+with open('input_day_1.txt', 'r') as puzzle_input:
+    for rotation in puzzle_input:
+        if rotation.startswith("L"):
+            current_position = (current_position - int(rotation.strip("L"))) % 100
+        if rotation.startswith("R"):
+            current_position = (current_position + int(rotation.strip("R"))) % 100
+        if current_position == 0:
+            zero_count += 1
+
+print(zero_count)
+
+
+
+
