49d90e54897285c55f06b71c0548dd66798b677a
Ajburdett <ab64@sanger.ac.uk>
2025-12-02 21:46:54 +0000

    day 2 part 1 done

--- day_2/part_1.py
+++ day_2/part_1.py
@@ -1,5 +1,23 @@
 
 
 with open('day_2/puzzle_input.txt', 'r') as puzzle_input:
-    for id_range in puzzle_input.read().strip().split(','):
-        continue
+    ranges = puzzle_input.read().strip().split(',')
+    answer = 0
+    for id_range in ranges:
+        start, end = map(int, id_range.split('-'))
+        for number in range(start, end + 1):
+            num_str = str(number)
+            if len(num_str) % 2 != 0:
+                continue
+
+            mid = len(num_str) // 2
+            first_half = str(int(num_str[:mid]))
+            second_half = str(int(num_str[mid:]))
+            if first_half == second_half:
+                answer += number
+    print(answer)
+
+
+        # for each number in the range
+        # split it in half - see if one half matches the other
+        # check for leading zeros
